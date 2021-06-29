49a65754d34468e8f23f4613c41d34bda0ed1df5
Caio Vieira <caiocesarav0208@gmail.com>
2021-06-29 19:00:26 -0300

    implement application logic

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -6,23 +6,48 @@ import more from '../../assets/more.svg'
 import './styles.scss'
 
 type TaskPropsType = {
+    tId: number,
     checked: boolean;
     task: string;
+    deleteTask: (taskId: number) => void;
+    updateTask: (taskId: number, updatedTask: string) => void;
 }
 
 export function Task(props: TaskPropsType) {
     const { buttonProps, itemProps, isOpen, setIsOpen } = useDropdownMenu(2);
     const [isChecked, setIsChecked] = useState(props.checked);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const [updateField, setUpdateField] = useState<string>(props.task);
+
+
 
     return (
         <div className={isChecked ? "task done" : "task"}>
             <img className="check" src={check} alt="Caixa marcada" onClick={() => { setIsChecked(!isChecked) }} />
             <div className="box" onClick={() => { setIsChecked(!isChecked) }}></div>
-            <p>{props.task}</p>
+
+            {isEditing ? (
+                <form onSubmit={(e) => e.preventDefault()}>
+                    <textarea onChange={(e) => { setUpdateField(e.target.value) }} value={updateField} />
+                    <div className="wrapper">
+                        <button className="cancel" onClick={() => {
+                            setIsEditing(false);
+                            setUpdateField(props.task)
+                        }} >Cancelar</button>
+                        <button onClick={(e) => {
+                            props.updateTask(props.tId, updateField)
+                            setIsEditing(false)
+                        }}>Salvar</button>
+                    </div>
+                </form>
+            ) :
+                (<p>{props.task}</p>)}
+
             <button {...buttonProps} className="options" onClick={() => setIsOpen(true)}><img src={more} alt="Opções da tarefa" /></button>
             <div className={isOpen ? "optionsBox visible" : "optionsBox"} role="options">
-                <p><a {...itemProps[0]} className="edit">Editar</a></p>
-                <p><a {...itemProps[1]} className="delete">Excluir</a></p>
+                <p onClick={() => setIsEditing(true)}><a {...itemProps[0]} className="edit">Editar</a></p>
+                <p onClick={() => props.deleteTask(props.tId)} ><a {...itemProps[1]} className="delete">Excluir</a></p>
             </div>
         </div>
     )
--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -4,27 +4,63 @@ import "./styles.scss"
 import { Task } from '../../components/task'
 import { useState } from "react"
 
-let tasklist = [
-    {
-        checked: false,
-        task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
-    },
-    {
-        checked: false,
-        task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
-    },
-    {
-        checked: false,
-        task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
-    },
-    {
-        checked: false,
-        task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
-    },
-]
+
 
 export function Tasks() {
+
+    const [tasklist, setTasklist] = useState([
+        {
+            id: 1,
+            checked: false,
+            task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
+        },
+        {
+            id: 2,
+            checked: false,
+            task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
+        },
+        {
+            id: 3,
+            checked: false,
+            task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
+        },
+        {
+            id: 4,
+            checked: false,
+            task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
+        },
+    ])
+
     const [taskField, setTaskField] = useState('');
+
+
+    function createTask(e: any) {
+        e.preventDefault();
+        if (taskField !== '') {
+            tasklist.push({
+                id: tasklist[tasklist.length - 1].id + 1,
+                task: taskField,
+                checked: false
+            })
+            setTaskField('');
+        }
+    }
+
+    function deleteTask(taskId: number) {
+        setTasklist(tasklist.filter(task => task.id !== taskId));
+    }
+    function updateTask(taskId: number, updatedTask: string) {
+        setTasklist(tasklist.map(task => {
+            if (task.id === taskId) {
+                return {
+                    id: task.id,
+                    task: updatedTask,
+                    checked: task.checked
+                }
+            }
+            return task;
+        }))
+    }
 
     return (
         <div id="tasks">
@@ -33,12 +69,20 @@ export function Tasks() {
                 <div className="user"></div>
             </header>
             <main>
-                <form >
+                <form onSubmit={(e) => { createTask(e) }} >
                     <label htmlFor="task">Nova tarefa:</label>
                     <input onChange={(e) => setTaskField(e.target.value)} value={taskField} type="text" name="task" id="newTask" />
                     <button type="submit">Adicionar</button>
                 </form>
-                {tasklist.map((task, index) => { return <Task key={`task_${index}`} checked={task.checked} task={task.task} /> })}
+                {tasklist.map((task, index) => {
+                    return <Task
+                        key={`task_${index}`}
+                        tId={task.id}
+                        deleteTask={deleteTask}
+                        checked={task.checked}
+                        updateTask={updateTask}
+                        task={task.task} />
+                })}
 
             </main>
         </div>
