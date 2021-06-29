13ef71682354f4981384f94a7c3e72a98bd7f212
Caio Vieira <caiocesarav0208@gmail.com>
2021-06-29 15:28:36 -0300

    add dropdown menu lib, begin create add new task functions

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,13 +1,29 @@
+import { useState } from 'react';
+import useDropdownMenu from 'react-accessible-dropdown-menu-hook';
 import check from '../../assets/check.svg'
+import more from '../../assets/more.svg'
 
 import './styles.scss'
-export function Task() {
+
+type TaskPropsType = {
+    checked: boolean;
+    task: string;
+}
+
+export function Task(props: TaskPropsType) {
+    const { buttonProps, itemProps, isOpen, setIsOpen } = useDropdownMenu(2);
+    const [isChecked, setIsChecked] = useState(props.checked);
 
     return (
-        <div className="task">
-            <img src={check} alt="Caixa marcada" />
-            <div className="box"></div>
-            <p>Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id.</p>
+        <div className={isChecked ? "task done" : "task"}>
+            <img className="check" src={check} alt="Caixa marcada" onClick={() => { setIsChecked(!isChecked) }} />
+            <div className="box" onClick={() => { setIsChecked(!isChecked) }}></div>
+            <p>{props.task}</p>
+            <button {...buttonProps} className="options" onClick={() => setIsOpen(true)}><img src={more} alt="Opções da tarefa" /></button>
+            <div className={isOpen ? "optionsBox visible" : "optionsBox"} role="options">
+                <p><a {...itemProps[0]} className="edit">Editar</a></p>
+                <p><a {...itemProps[1]} className="delete">Excluir</a></p>
+            </div>
         </div>
     )
 }
--- src/pages/Tasks/index.tsx
+++ src/pages/Tasks/index.tsx
@@ -2,8 +2,29 @@
 import "./styles.scss"
 
 import { Task } from '../../components/task'
+import { useState } from "react"
+
+let tasklist = [
+    {
+        checked: false,
+        task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
+    },
+    {
+        checked: false,
+        task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
+    },
+    {
+        checked: false,
+        task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
+    },
+    {
+        checked: false,
+        task: "Marcação para tarefa Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, ea ut. Accusamus earum quasi nulla fugiat quia, quaerat, vitae eum commodi adipisci voluptas reprehenderit provident, blanditiis ipsa. Eum, eius id."
+    },
+]
 
 export function Tasks() {
+    const [taskField, setTaskField] = useState('');
 
     return (
         <div id="tasks">
@@ -14,13 +35,11 @@ export function Tasks() {
             <main>
                 <form >
                     <label htmlFor="task">Nova tarefa:</label>
-                    <input type="text" name="task" id="newTask" />
+                    <input onChange={(e) => setTaskField(e.target.value)} value={taskField} type="text" name="task" id="newTask" />
                     <button type="submit">Adicionar</button>
                 </form>
-                <Task />
-                <Task />
-                <Task />
-                <Task />
+                {tasklist.map((task, index) => { return <Task key={`task_${index}`} checked={task.checked} task={task.task} /> })}
+
             </main>
         </div>
     )
