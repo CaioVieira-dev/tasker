fdbf647dd1050dd844b250c59d67c6481e98f5d8
Caio Vieira <caiocesarav0208@gmail.com>
2021-06-30 13:32:29 -0300

    style login page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div id="App">
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Tasks} />
